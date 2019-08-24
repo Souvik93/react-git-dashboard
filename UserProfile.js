4e2599363e0c1ec1276c4ecfb3f3fb54db95094a
Souvik93 <souvik.das@live.in>
2019-08-24 15:36:24 +0530

    2 files modified

--- UserProfile.js
+++ UserProfile.js
@@ -1,32 +1,67 @@
 
 import React, { Component } from 'react';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import ImageIcon from '@material-ui/icons/Image';
+import WorkIcon from '@material-ui/icons/Work';
+import PlaceIcon from '@material-ui/icons/Place';
 
 export default class UserProfile extends Component {
-  render () {
+  render() {
     return (
       <div>
         <section>
-        <div className="col-sm-4">
-          <img
-            src={this.props.userDetails.avatar_url}
-            className="rounded-circle img-responsive"
-            alt="User Avatar"
-          />
+          <div className="col-md-4">
+            <img
+              src={this.props.userDetails.avatar_url}
+              className="rounded-circle img-responsive"
+              alt="User Avatar"
+            />
           </div>
-          <div className="col-sm-8">
-          <h4>
-            <span> {this.props.userDetails.name} </span>
-          </h4>
-          <h4>
-            
-             <span> {this.props.userDetails.login} </span>
-          </h4>
-          <h4>
-            <span> {this.props.userDetails.bio} </span>
-          </h4>
-          <h4>
-            <span> {this.props.userDetails.location} </span>
-          </h4>
+          <div className="col-md-8">
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <ImageIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="Name" secondary={this.props.userDetails.name} />
+            </ListItem>
+            <Divider variant="inset" />
+
+              <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <WorkIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="Work" secondary={this.props.userDetails.company} />
+            </ListItem>
+
+            <Divider variant="inset"/>
+
+              <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <ImageIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="Bio" secondary={this.props.userDetails.bio} />
+            </ListItem>
+
+            <Divider variant="inset" />
+
+              <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <PlaceIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="Location" secondary={this.props.userDetails.location} />
+            </ListItem>
           </div>
         </section>
       </div>
