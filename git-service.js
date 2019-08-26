2ef2a292e59daf7c2eba2bfe5025245c90a4fca1
Souvik93 <souvik.das@live.in>
2019-08-26 18:40:57 +0530

    1 file modified

--- git-service.js
+++ git-service.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 const BASE_URL = 'https://api.github.com';
 
-export {getUserData}
+export {getUserData,getRepoData}
 
 function getUserData(userName) {
   return axios.get(`${BASE_URL}/users/${userName}`);
+}
+function getRepoData(username) {
+   const url = `${BASE_URL}/users/${username}/repos?per_page=250`;
+  return axios.get(url);
 }
